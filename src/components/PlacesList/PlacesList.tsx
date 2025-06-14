2d12699edfcb73b60de06b2dcd4740d9254d85c8
Md Hilwan <wan.wanted.1@gmail.com>
2025-06-13 23:50:23 -0500

    refactor size

--- src/components/PlacesList/PlacesList.tsx
+++ src/components/PlacesList/PlacesList.tsx
@@ -9,7 +9,7 @@ import 'swiper/css/navigation';
 
 export default function PlacesList() {
   const {placesData} = useMapContext();
-  return <div className="mx-auto md:m-0 md:w-1/2 h-[100vh] overflow-y-auto p-4 md:border-l">
+  return <div className="mx-auto md:m-0 md:w-1/2 h-[100vh] overflow-y-auto sm:p-4 md:border-l">
     <h2 className="text-lg font-semibold mb-4">Saved Places</h2>
     <ul className="grid grid-cols-1 sm:grid-cols-2 grid-cols-1 gap-4">
       {placesData.map((place, index) => (
